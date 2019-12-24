26a490ba09c9a59add8df384135a98b7ef148ab2
jin.qu <qj1180405@163.com>
2019-12-24 09:06:37 +0800

    First commit.

---
+++ src/main/java/com/demon/springboot/SpringBootGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.demon.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demon/springboot/SpringBootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
